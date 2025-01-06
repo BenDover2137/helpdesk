5de9c32e19be5f90a395a7d061ea07dba9e85964
leon <costam@gmail.com>
2025-01-06 20:12:54 +0100

    poprawki

--- tickets/forms.py
+++ tickets/forms.py
@@ -3,6 +3,28 @@ from .models import Ticket, Comment, ProblemType
 from django.contrib.auth.models import Group
 
 from django.contrib.auth.models import Group
+class TicketStatusForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['status']
+
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)  # Get the user from the form kwargs
+        super().__init__(*args, **kwargs)
+
+        # Restrict status options based on the user's role
+        if self.user and self.user == self.instance.created_by:
+            # Ticket creator can only set status to 'open'
+            self.fields['status'].choices = [
+                ('open', 'Open'),
+            ]
+        elif self.user and self.user.groups.filter(name='Tech Support').exists():
+            # Tech Support users can set status to 'in_progress' or 'closed'
+            self.fields['status'].choices = [
+                ('in_progress', 'In Progress'),
+                ('closed', 'Closed'),
+            ]
 
 class TicketForm(forms.ModelForm):
     class Meta:
